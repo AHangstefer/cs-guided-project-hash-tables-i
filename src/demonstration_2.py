c65d79b61b407ed2bf3bec09eaa1239436d3f39a
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-03-23 21:38:48 -0400

    ahhhh

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -44,6 +44,29 @@ Notes:
 
 - order.length == 26
 - All characters in words[i] and order are English lowercase letters.
+
+UPPER
+
+input: array of words
+str of chars in some order 
+    -always 26 
+    - no duplicates
+output: boolean
+
+Loops over each word
+compare current word to next word
+    -letter by letter
+    -loop
+    -find index in "order" (letters in alpha order)
+    -find(), index, or loop over order
+    -index_1, index_2
+    -if index_1 <= index_2:
+        continue
+    -else return false
+if sorted:
+    continue
+else:
+    return false
 """
 def are_words_sorted(words, alpha_order):
     """
@@ -54,5 +77,41 @@ def are_words_sorted(words, alpha_order):
     Output:
     bool
     """
-    # Your code here
+    alpha_dict = {}
+    for i in range(len(alpha_order)):
+        alpha_dict[alpha_order[i]] = i
+    print(alpha_dict)
+
+
+    # Loop over the array of words
+    for i in range(len(words) -1):
+        #look at two words at a time
+        #word1, word2
+        word1 = words[i]
+        word2 = words[i +1]
+        #compare word1 to word2
+        #Go letter by letter of both words
+        for j in range(min(len(word1), len(word2))):
+            #letter1, letter2
+            letter1 = word1[j]
+            letter2 = word2[j]
+            #find if letter1< letter2 using the apha_order
+            if letter1 != letter2:
+                if alpha_dict[letter1] > alpha_dict[letter2]:
+                    return False
+                    #break here
+                break
+            #If we got to the end of the loop and we didn't break, check the length of the words
+        else:
+            if len(word1) > len(word2):
+                return False
+                    #get index of letter1, get index of letter2
+    return True
+
+
+words = ["lambda", "school"]
+order = "hlabcdefgijkmnopqrstuvwxyz"
+print(are_words_sorted(words, order))
+
+
 
